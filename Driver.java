b458e5647ff0c129b11245150c4f667adbef81a3
Trey-jpg <56291093+Trey-jpg@users.noreply.github.com>
2020-09-10 03:18:19 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 public class Driver 
 {
     public static void main(String[] args)
     {
         int count = 0;
-        String s = "Habakkuk";
+        Scanner vowelCounter = new Scanner(System.in);
+        System.out.println("Enter a word to find how many vowels.");
+
+        String s = vowelCounter.nextLine();
+        System.out.println(count);
+        
         for(int i = s.length()-1; i >= 0; i--)
         {
             if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u')
@@ -12,5 +18,6 @@ public class Driver
             }
         }
         System.out.print(count);
+
     }
 }
